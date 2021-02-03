ee569cdfb1e20b50e3144c477de928108302263f
Kapil Garg <55026503+kapilgarg16@users.noreply.github.com>
2021-02-03 11:40:01 -0800

    Add files via upload

---
+++ Menu system/app.js
@@ -0,0 +1,145 @@
+
+//use reduce for change category after change in mennu
+
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "Breakfast",
+      price: 15.99,
+      img: "item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "Lunch",
+      price: 13.99,
+      img: "item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "Shakes",
+      price: 6.99,
+      img: "item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "Breakfast",
+      price: 20.99,
+      img: "item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "Lunch",
+      price: 22.99,
+      img: "item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "Shakes",
+      price: 18.99,
+      img: "item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "Breakfast",
+      price: 8.99,
+      img: "item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "Lunch",
+      price: 12.99,
+      img: "item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "Shakes",
+      price: 16.99,
+      img: "item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
+
+const sectionCenter = document.querySelector(".section-center");
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+// console.log(filterBtns);
+// var textOnButton =  filterBtns.textContent;
+
+// console.log(textOnButton);
+// displayButton(textOnButton);
+
+window.addEventListener("DOMContentLoaded", function(){
+           displayMenuItems(menu);
+});
+
+filterBtns.forEach(function(btn){
+    // console.log(btn)
+    btn.addEventListener("click",function(e){
+        const category = e.currentTarget.dataset.id;
+        // console.log(category);
+        // now use filter 
+        const menuCategory = menu.filter(function(menuItem){
+            // console.log(menuItem);
+            if(menuItem.category === category){
+            return menuItem;
+            }
+        });
+        if(category === "All")
+        {
+            displayMenuItems(menu)
+        }
+        else
+        {
+            // console.log("kapil");
+            displayMenuItems(menuCategory);
+            // console.log(menuCategory);
+        }
+    });
+});
+    
+
+
+function displayMenuItems(menu)
+{
+ 
+    let displayMenu = menu.map(function(item){ //store the element from menu to map
+        // console.log(item);
+
+        return `<article class="menu-item">
+                    
+        <img src=${item.img} class="photo"
+        alt=${item.title} />
+
+        <div class="item-info">
+            <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+            </header>
+            <p><strong>${item.category}</strong></p>
+            <p class="item-text">${item.desc}</p>
+        </div>
+
+    </article>`
+    })
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
